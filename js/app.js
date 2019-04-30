47fb527b316ac359553483425032e70d01a37552
sarahduv <sarah.duvdevani@gmail.com>
2019-04-30 16:44:42 -0700

    This is my first committ after editing the code

---
+++ js/app.js
@@ -0,0 +1,50 @@
+var answerOne = prompt('Am I 99 years old?').toLowerCase();
+
+if (answerOne === 'no' || answerOne === 'n') {
+  alert('You are correct! I am not 99 years old!');
+  console.log('The user got the correct answer for #1');
+} else {
+  alert('You are wrong! I am not 99 years old!');
+  console.log('The user got the wrong answer for #1');
+}
+
+var answerTwo = prompt('Am I from North Carolina?').toLowerCase();
+
+if (answerTwo === 'yes' || answerTwo === 'y') {
+  alert('You are correct! I am from North Carolina!');
+  console.log('The user got the correct answer for #1');
+} else {
+  alert('You are wrong! I am not from North Carolina!');
+  console.log('The user got the wrong answer for #1');
+}
+
+var answerThree = prompt('Is my name Snickerdoodle?').toLowerCase();
+
+if (answerThree === 'no' || answerThree === 'n') {
+  alert('You are correct! my name is not Snickerdoodle!');
+  console.log('The user got the correct answer for #1');
+} else {
+  alert('You are wrong! My name is not SNickerdoodle!');
+  console.log('The user got the wrong answer for #1');
+}
+
+var answerFour = prompt('Do I have three arms?').toLowerCase();
+
+if (answerFour === 'no' || answerFour === 'n') {
+  alert('You are correct! I only have two arms!');
+  console.log('The user got the correct answer for #1');
+} else {
+  alert('You are wrong! Sometimes I wish I had three arms!');
+  console.log('The user got the wrong answer for #1');
+}
+
+var answerFive = prompt('Am I a cyclops?').toLowerCase();
+
+if (answerFive === 'no' || answerFive === 'n') {
+  alert('You are correct! No cyclops here!');
+  console.log('The user got the correct answer for #1');
+} else {
+  alert('You are wrong! What a goober you are!');
+  console.log('The user got the wrong answer for #1');
+}
+
