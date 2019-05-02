8c5fd3dd3327e77b3336a211e7c961ce0e4491d0
sarahduv <sarah.duvdevani@gmail.com>
2019-05-02 15:50:35 -0700

    This contains fixes for a resubmit of lab 3.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,35 @@
 'use strict';
 
+//This var stores the total number of correct responses throughout the duration of questions
 var correctResponses = 0;
 
+
+//This var is requesting the user's name
+var userName = prompt('Hi there! What is your name?');
+console.log(userName);
+
+alert('Alright ' + userName + ', let\'s play!');
+
+
+//Question one
 function questionOne(){
-var answerOne = prompt('Am I 99 years old?').toLowerCase();
+  var answerOne = prompt('Am I 99 years old?').toLowerCase();
 
   if (answerOne === 'no' || answerOne === 'n') {
-    alert('You are correct! I am not 99 years old!');
+    alert('You are correct, ' + userName + '! I am not 99 years old!');
     correctResponses++;
     console.log('The user got the correct answer for #1');
   } else {
-    alert('You are wrong! I am not 99 years old!');
+    alert('You are wrong, ' + userName + '! I am not 99 years old!');
     console.log('The user got the wrong answer for #1');
   } 
 }
 questionOne();
 
+
+//Question two
 function questionTwo() {
-var answerTwo = prompt('Am I from North Carolina?').toLowerCase();
+  var answerTwo = prompt('Alright ' + userName + ', am I from North Carolina?').toLowerCase();
 
   if (answerTwo === 'yes' || answerTwo === 'y') {
     alert('You are correct! I am from North Carolina!');
@@ -30,82 +42,91 @@ var answerTwo = prompt('Am I from North Carolina?').toLowerCase();
 }
 questionTwo();
 
+
+//Question three
 function questionThree(){
-  var answerThree = prompt('Is my name Snickerdoodle?').toLowerCase();
+  var answerThree = prompt('Alright ' + userName + ', I bet you can\'t guess this one! Is my name Snickerdoodle?').toLowerCase();
 
   if (answerThree === 'no' || answerThree === 'n') {
-    alert('You are correct! my name is not Snickerdoodle!');
+    alert('You are correctemundo! my name is not Snickerdoodle!');
     correctResponses++;
     console.log('The user got the correct answer for #1');
   } else {
-    alert('You are wrong! My name is not Snickerdoodle!');
+    alert('You are wrong! How about instead of your name being ' + userName + ', you now call yourself Snickerdoodle!');
     console.log('The user got the wrong answer for #1');
   }
 }
 questionThree();
 
+
+//Question four
 function questionFour(){
-var answerFour = prompt('Do I have three arms?').toLowerCase();
+  var answerFour = prompt('Do I have three arms? Think really hard on this one ' + userName + '...').toLowerCase();
 
   if (answerFour === 'no' || answerFour === 'n') {
     alert('You are correct! I only have two arms!');
     correctResponses++;
     console.log('The user got the correct answer for #1');
   } else {
-    alert('You are wrong! Sometimes I wish I had three arms!');
+    alert(userName + ' you are so very wrong. Although sometimes I wish I had three arms!');
     console.log('The user got the wrong answer for #1');
   }
 }
 questionFour();
 
+
+//Question five
 function questionFive(){
-  var answerFive = prompt('Am I a cyclops?').toLowerCase();
+  var answerFive = prompt('FYI: You are not allowed to get this one wrong. Am I a cyclops?').toLowerCase();
 
   if (answerFive === 'no' || answerFive === 'n') {
-    alert('You are correct! No cyclops here!');
+    alert('You are correct, ' + userName + '! No cyclops here!');
     correctResponses++;
     console.log('The user got the correct answer for #1');
   } else {
-    alert('You are wrong! What a goober you are!');
+    alert('You are wrong! What a goober you are ' + userName + '!');
     console.log('The user got the wrong answer for #1');
   }
 }
 questionFive();
 
+
+//Question six
 function questionSix(){
   var tries = 5;
   var secretNumber = 17;
 
-
   while (tries > 0 && answerSix !== secretNumber) {
-    var answerSix = Number(prompt('What is my favorite number? Hint: it\'s a number between 0 and 20.'));
+    var answerSix = Number(prompt(userName + ', do you know what my favorite number is? Hint: it\'s a number between 0 and 20.'));
     console.log(answerSix);
     tries--;
     if (answerSix === secretNumber) {
       alert('Correct! You guessed my favorite number!');
       correctResponses++;
-      break;
       console.log('The user guessed right.');
+      break;
     } else if (answerSix > secretNumber) {
-      alert('No! That is not my favorite number. Guess lower! You have ' + tries + ' tries left!');
+      alert('No! That is not my favorite number. Guess lower ' + userName + '! You have ' + tries + ' tries left!');
       console.log('The user guessed too high.');
     } else if (answerSix < secretNumber) {
-      alert('No! That is not my favorite number. Guess higher! You have ' + tries + ' tries left!');
+      alert('No! That is not my favorite number. Guess higher ' + userName + '! You have ' + tries + ' tries left!');
       console.log('The user did not guess correctly and ran out of tries.');
     }
     if (tries === 0) {
-      alert('You did not guess it right and you are out of tries!');
+      alert('Womp womp womp.. you failed in epic fashion. FYI, my favorite number is ' + secretNumber + '.');
     }
   }
 }
 questionSix();
 
+
+//Question seven
 function questionSeven(){
   var attempts = 6;
   var cities = ['carthage', 'wilmington', 'leland', 'moon'];
 
   while (attempts > 0) {
-    var answerSeven = prompt('What other city have I lived in other than Seattle?').toLowerCase();
+    var answerSeven = prompt('So ' + userName + ', can you guess one other city I have lived in other than Seattle?').toLowerCase();
     console.log(answerSeven);
     attempts--;
     if (cities.indexOf(answerSeven) >= 0) {
@@ -118,7 +139,7 @@ function questionSeven(){
       console.log('The user did not guess correctly.');
     }
     if (attempts === 0) {
-      alert('You have run out of tries. I have lived in: Carthage, Wilmington, Leland, Seattle, and the Moon! (Just kidding, not the moon!)');
+      alert('Admittedly, I have not lived in very many places, so that may have been too difficult to guess. I have lived in: Carthage, Wilmington, Leland, Seattle, and the Moon! (Just kidding, not the moon!)');
       console.log('The user did not guess correctly and ran out of tries.');
       break;
     }
@@ -126,10 +147,15 @@ function questionSeven(){
 }
 questionSeven();
 
-alert('You have gotten ' + correctResponses + ' questions correct out of 7!');
 
-
-
+//This returns the value of correct responses collected
+if (correctResponses === 7) {
+  alert('You have gotten all seven questions correct! That is epic and you deserve a beautiful gold star!');
+} else if(correctResponses < 7 && correctResponses > 3) {
+  alert('You have gotten ' + correctResponses + ' questions correct out of 7! That is pretty darn good!');
+} else if(correctResponses < 4) {
+  alert('You did\'t do so hot there. Maybe you should try again.');
+}
 
 
 
